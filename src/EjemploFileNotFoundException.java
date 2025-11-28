24128e0018918a419bb331a6f5a55311c41f15dc
AnaClaudiaIR <anac.ihnrod@educa.jcyl.es>
2025-11-28 23:41:31 +0100

    subir2

--- src/EjemploFileNotFoundException.java
+++ src/EjemploFileNotFoundException.java
@@ -1,13 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class EjemploFileNotFoundException {
+    public static void main(String[] args) {
+
+        //Archivo que se va a intentar abrir
+        String nombreArchivo = "Hola.txt";
+        try {
+            //Crear objeto File --> Ubicación del archivo
+            File archivo = new File(nombreArchivo);
+
+            //Se comunica con el SO --> Intenta establecer conexión con el archivo
+            Scanner lector = new Scanner(archivo);
+            System.out.println("Se ha encontrado el archivo.");
+
+            //Imprimir la primera frase del documento
+            if (lector.hasNextLine()) {
+                System.out.println("Contenido de la primera línea: " + lector.nextLine());
+            }
+            lector.close(); //Cerrar el scanner
+        } catch (FileNotFoundException e) {
+            System.err.println("El archivo '" + nombreArchivo + "' no fue encontrado.");
+            System.err.println("Detalles del error: " + e.getMessage());
+        }
+    }
 }
